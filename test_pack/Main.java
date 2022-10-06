ac1ba181bfe50e40af156ef3f50dc5a192470753
HHNebula <sungsoomr@icloud.com>
2022-10-06 12:09:34 +0900

    Test

--- test_pack/Main.java
+++ test_pack/Main.java
@@ -1,9 +1,18 @@
 
 package test_pack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-        // Test Commit
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        br.close();
+
     }
 }
