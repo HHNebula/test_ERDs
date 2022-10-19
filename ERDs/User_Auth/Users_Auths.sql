bbbec7f1ce1821d9a322d8f50df2a9347c7e0a64
HHNebula <sungsoomr@icloud.com>
2022-10-19 15:23:45 +0900

    Update

--- ERDs/User_Auth/Users_Auths.sql
+++ ERDs/User_Auth/Users_Auths.sql
@@ -1,16 +1,10 @@
 
-CREATE TABLE Auth_Names
+CREATE TABLE Auths
 (
   Auth_ID   VARCHAR(200) NOT NULL COMMENT '권한 대표값',
   Auth_Name VARCHAR(200) NULL     COMMENT '접근 권한명',
   PRIMARY KEY (Auth_ID)
 ) COMMENT '권한';
-
-CREATE TABLE Auths
-(
-  User_ID VARCHAR(200) NOT NULL COMMENT '사용자 대표값',
-  Auth_ID VARCHAR(200) NOT NULL COMMENT '권한 대표값'
-) COMMENT '사용자별 권한';
 
 CREATE TABLE Users
 (
@@ -21,12 +15,18 @@ CREATE TABLE Users
   PRIMARY KEY (User_ID)
 ) COMMENT '사용자';
 
-ALTER TABLE Auths
-  ADD CONSTRAINT FK_Users_TO_Auths
+CREATE TABLE Users_Auths
+(
+  User_ID VARCHAR(200) NOT NULL COMMENT '사용자 대표값',
+  Auth_ID VARCHAR(200) NOT NULL COMMENT '권한 대표값'
+) COMMENT '사용자별 권한';
+
+ALTER TABLE Users_Auths
+  ADD CONSTRAINT FK_Users_TO_Users_Auths
     FOREIGN KEY (User_ID)
     REFERENCES Users (User_ID);
 
-ALTER TABLE Auths
-  ADD CONSTRAINT FK_Auth_Names_TO_Auths
+ALTER TABLE Users_Auths
+  ADD CONSTRAINT FK_Auths_TO_Users_Auths
     FOREIGN KEY (Auth_ID)
-    REFERENCES Auth_Names (Auth_ID);
+    REFERENCES Auths (Auth_ID);
